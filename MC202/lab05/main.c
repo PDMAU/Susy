f26adc06c7475e48c363b3af76fdb4a2c8e1b3db
Maurilio Dos Santos Goncalves <ra203648@dias.lab.ic.unicamp.br>
2018-08-30 17:10:59 -0300

    mend

---
+++ MC202/lab05/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Participante{
+    char nome[30];
+    int telefone;
+};
+
+void lerDadosParticipante(struct Participante* participantes){
+}
+
+
+int main()
+{
+    struct Participante participantes[4];
+    printf("Hello world!\n");
+
+    return 0;
+}
+
+
