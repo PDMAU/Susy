a14b15a29537d39629aa23181d8f17d4ab835c40
santosg <santosg@cpqd.com.br>
2018-11-23 16:53:33 -0200

    commit

--- MC202/lab14/lab14.c
+++ MC202/lab14/lab14.c
@@ -1,14 +1,35 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int **pessoas;// primeiro campo é a sua idade, o segundo é se esta entediado 0 -Nao, 1- Sim
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
 
 
 int calcularTriangulos(){
-	for(f = grafo; f != NULL; f = f ->prox){
-		for(g = f ->adjacencia; g != NULL; g = g->prox){
-			for(h = acharElemento(g->v); h != NULL; h = h->prox){
-				if(tem_aresta(grafo, h->v, f->n))
+	int f;
+	for(f = 0; f < n; f++){
+		for(g = p_grafo[f]->adjacencia; g != NULL; g = g->prox){
+			
+			for(h = p_grafo[g->v]->adjacencia; h != NULL; h = h->prox){
+				if(tem_aresta(p_grafo, f, h->v)){
+					index = calcularSeTemEntediado(p_grafo[f]->n,g->v,h->v);					
+				}
 			}		
 		}
 	}
+}
 
+int calcularSeTemEntediado(p_no f,p_no g, p_no h){
+	int media = (f->v+g->v+h->v)/3;
 
+	if(media > f->v || media > g->v || media > h->v){
+		return 1;
+	}
+
+	return 0;
 }
