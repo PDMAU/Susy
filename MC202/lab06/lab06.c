8fa3ec141972af8f068f84adf7e85bb15952f64b
santosg <santosg@cpqd.com.br>
2018-11-29 16:57:02 -0200

    commit 6

--- MC202/lab06/lab06.c
+++ MC202/lab06/lab06.c
@@ -6,15 +6,17 @@
 int main() {
     p_professor* professores;
     p_aluno* alunos;
-    int numeroProfessores,numeroAlunos,numeroDisciplinas, i,j;
+    int numeroProfessores, numeroAlunos, numeroDisciplinas, i, j;
     double salario, nota, maiorNota, menorNota;
     char nome[MAX_NOME], sobrenome[MAX_NOME], disciplina[MAX_CODIGO];
+    /*Leitura dos Professores*/
     scanf("%d", &numeroProfessores);
     professores = malloc(sizeof(Professor)*numeroProfessores);
     for(i=0; i<numeroProfessores; i++) {
         scanf("%s %s %lf %s", nome, sobrenome, &salario, disciplina);
         professores[i] = criarProfessor(nome, sobrenome, salario, disciplina);
     }
+    /*Leitura dos dados dos alunos e notas*/
     scanf("%d", &numeroAlunos);
     alunos = malloc(sizeof(Aluno)*numeroAlunos);
     for(i=0; i<numeroAlunos; i++) {
@@ -26,10 +28,10 @@ int main() {
             adicionarDisciplina(alunos[i], disciplina, nota);
         }
     }
+    /*Processa os dados para calcular o reajuste dos salarios*/
     for(i=0; i<numeroProfessores; i++) {
         strcpy(disciplina, professores[i]->disciplina);
         obterNotasExtremas(alunos,numeroAlunos,disciplina, &menorNota, &maiorNota);
-        printf("Maior: %lf Menor:%lf \n", maiorNota, menorNota);
         reajusteSalario(professores[i],(maiorNota+menorNota)/2.0);
         imprimirProfessor(professores[i]);
     }
---
+++ MC202/lab06/main - Cópia.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "universidade.h"
+#include <string.h>
+
+int main() {
+    p_professor* professores;
+    p_aluno* alunos;
+    int numeroProfessores, numeroAlunos, numeroDisciplinas, i, j;
+    double salario, nota, maiorNota, menorNota;
+    char nome[MAX_NOME], sobrenome[MAX_NOME], disciplina[MAX_CODIGO];
+    /*Leitura dos Professores*/
+    scanf("%d", &numeroProfessores);
+    professores = malloc(sizeof(Professor)*numeroProfessores);
+    for(i=0; i<numeroProfessores; i++) {
+        scanf("%s %s %lf %s", nome, sobrenome, &salario, disciplina);
+        professores[i] = criarProfessor(nome, sobrenome, salario, disciplina);
+    }
+    /*Leitura dos dados dos alunos e notas*/
+    scanf("%d", &numeroAlunos);
+    alunos = malloc(sizeof(Aluno)*numeroAlunos);
+    for(i=0; i<numeroAlunos; i++) {
+        scanf("%s %s", nome, sobrenome);
+        alunos[i] = criarAluno(nome, sobrenome);
+        scanf("%d", &numeroDisciplinas);
+        for(j=0; j < numeroDisciplinas; j++) {
+            scanf("%s %lf", disciplina, &nota);
+            adicionarDisciplina(alunos[i], disciplina, nota);
+        }
+    }
+    /*Processa os dados para calcular o reajuste dos salarios*/
+    for(i=0; i<numeroProfessores; i++) {
+        strcpy(disciplina, professores[i]->disciplina);
+        obterNotasExtremas(alunos,numeroAlunos,disciplina, &menorNota, &maiorNota);
+        reajusteSalario(professores[i],(maiorNota+menorNota)/2.0);
+        imprimirProfessor(professores[i]);
+    }
+    return 0;
+}
